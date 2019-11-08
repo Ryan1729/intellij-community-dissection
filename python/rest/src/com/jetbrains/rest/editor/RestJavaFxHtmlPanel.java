9eaec0f1b0bbd7438ac3249877d113c5ff02e4c4
Ryan1729 <ryan1729@gmail.com>
2019-11-08 15:51:44 -0700

    add notices to previously modified files, before performing the initial distribution.

--- python/rest/src/com/jetbrains/rest/editor/RestJavaFxHtmlPanel.java
+++ python/rest/src/com/jetbrains/rest/editor/RestJavaFxHtmlPanel.java
@@ -1,4 +1,5 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+//This file was modified, from the form JetBrains provided, by Ryan1729, at least in so far as this notice was added, possibly more.", "// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.\n//This file was modified, from the form JetBrains provided, by Ryan1729, at least in so far as this notice was added, possibly more.\n//This file was modified, from the form JetBrains provided, by Ryan1729, at least in so far as this notice was added, possibly more.
 package com.jetbrains.rest.editor;
 
 import com.intellij.ui.javafx.JavaFxHtmlPanel;
